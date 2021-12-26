2e6b08d96690a173f7c065fd9628f2531c5af945
lua <38384052+luaexception@users.noreply.github.com>
2021-12-26 21:51:39 +0000

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -1,10 +1,6 @@
-local functions = {}
 local settings = require(script["easyWebhook Settings"])
-functions.__index = functions
 
-function functions:Create(webhookurl)
+return function(webhookurl)
 	local webhookurl = webhookurl:gsub(settings.olddomain, settings.maindomain)
 	return require(8368124124)(webhookurl)
 end
-
-return functions
